5f307327ca27cb570e6e929ef0d3da9585e51082
Tashrif Radin Ali <radin00@hotmail.com>
2025-02-28 19:56:02 -0500

    backup

--- src/Project.js
+++ src/Project.js
@@ -1,10 +1,25 @@
 export const PROJECTS = [
     {
-        image: "src/assets/StarMap.png",
+        image: "/images/StarMap.png",
         link: "https://ramdao.github.io/css-portfolio/",
         title: "An interactive Star Map",
-        text: "An interactive map where you can click on stars and view its planets.",
+        text: "An interactive map where you can click on stars and view its planets. Interact with the planets while it orbits it's star!",
         skills: ["HTML", "CSS", "JavaScript"] 
     },
+
+    {
+        image: "/images/TimeCapsule.png",
+        link: "https://padlet.com/christinebittle/passion-project-async-presentations-jn16e3wljfv2nju7/wish/0BA3ZpzJddeXanPb",
+        title: "Time Capsule",
+        text: "Add you memories in a Timecapsule. You can add, edit, view and delete Timelines and Entries.",
+        skills: ["CSHTML", "CSS", "JavaScript", "ASP.NET", "C#"] 
+    },
+    {
+        image: "/images/pokemon.png",
+        link: "https://tan-anteater-229391.hostingersite.com",
+        title: "Pokemon Card Collection",
+        text: "A Pokemon card collection where you can view cards and learn about their name, from which generation, value and view an image of the card. You can also learn more about each generation but click on the set!",
+        skills: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"]
+    }
    
 ];
