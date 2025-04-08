bb4ce149c90cfa17cc853dc3f10fb5e3c1c1fcf3
Tashrif Radin Ali <radin00@hotmail.com>
2025-04-07 21:42:15 -0400

    backup

--- src/Project.js
+++ src/Project.js
@@ -20,6 +20,13 @@ export const PROJECTS = [
         title: "Pokemon Card Collection",
         text: "A Pokemon card collection where you can view cards and learn about their name, from which generation, value and view an image of the card. You can also learn more about each generation but click on the set!",
         skills: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"]
+    },
+    {
+        image: "/images/hackhaton.png",
+        link: "https://www.reddit.com/r/technicallyMemegame/",
+        title: "Hackathon Meme Game",
+        text: " Hackathon/ Hack Reddit 2025. A collaboration with 1 other person. A game where you vote for the best meme. Get a score best on the votes you put in vs the most popular vote ",
+        skills: ["Typescript", "React", "Redis", "Devvit"]
     }
    
 ];
