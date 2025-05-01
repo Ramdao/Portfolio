2dd310db1fb873e848694c31f5ddc750afeb12cd
Tashrif Radin Ali <radin00@hotmail.com>
2025-05-01 14:30:48 -0400

    add and delete projects

--- src/Project.js
+++ src/Project.js
@@ -4,7 +4,7 @@ export const PROJECTS = [
         link: "https://ai-recipe-generator-search-1.onrender.com",
         title: "Tackling food wastage in Canada (Walmart) ",
         text: "Walmart in Canada has a problem with food wastage. This project aims to tackle that problem by creating 2 features that can be added to the Walmart app and promoting food surplus. The first feature uses AI to generate recipes for users based on what the user on the surplus food. The second features uses QR code scanner to scan the food and generate a recipe using AI.",
-        skills: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"]
+        skills: ["HTML", "CSS", "JavaScript", "OpenAI", "Node.js"]
     },
     {
         image: "/images/StarMap.png",
@@ -35,7 +35,13 @@ export const PROJECTS = [
         text: "A Pokemon card collection where you can view cards and learn about their name, from which generation, value and view an image of the card. You can also learn more about each generation but click on the set!",
         skills: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"]
     },
-  
+    {
+        image: "/images/GameViewer.png",
+        link: "https://slategrey-hedgehog-104722.hostingersite.com",
+        title: "Game Viewer",
+        text: "A game viewer where you can view games and players and view their details. This application Uses two custom APIs to fetch the data on games and players.",
+        skills: ["React", "CSS", "MongoDB", "Node.js", "JavsaScript"]
+    },
     {
         image: "/images/TimeCapsule.png",
         link: "https://www.youtube.com/watch?v=VuQMQX6yjkI",
@@ -43,12 +49,6 @@ export const PROJECTS = [
         text: "Add your memories in a Timecapsule. You can add a memory by creating a timline and adding a entry to it. Within the entry you can add description,location and images. You can perform CRUD operations on the entries and timelines.",
         skills: ["CSHTML", "CSS", "JavaScript", "ASP.NET", "C#"] 
     },
-    {
-        image: "/images/hackhaton.png",
-        link: "https://www.reddit.com/r/technicallyMemegame/",
-        title: "Hackathon Meme Game",
-        text: " Hackathon/ Hack Reddit 2025. A collaboration with 1 other person. A game where you vote for the best meme. Get a score best on the votes you put in vs the most popular vote ",
-        skills: ["Typescript", "React", "Redis", "Devvit"]
-    }
+ 
    
 ];
