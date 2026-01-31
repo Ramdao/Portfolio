52f6a46c959724a0e2d3f7ed8cc0996d78e99ba6
Tashrif Radin Ali <radin00@hotmail.com>
2026-01-31 16:13:15 +0900

    clean

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-export default function Button({children,href}){
-    var handleClick =()=>{
-        window.open(href, "_blank", "noopener,noreferrer");
-    }
-    return <>
-    <button className="button" onClick={handleClick}>{children}</button>
-    
-    </>
-}
